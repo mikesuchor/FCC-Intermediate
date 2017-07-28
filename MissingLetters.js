e2193f416302f8b97aa486e197fe133a41ee5069
mikesuchor@gmail.com <mikesuchor@gmail.com>
2017-07-28 00:23:14 -0400

    FCC Intermediate Javascript Exercises

--- MissingLetters.js
+++ MissingLetters.js
@@ -5,15 +5,22 @@ If all letters are present in the range, return undefined.
 
 function fearNotLetter(str) {
 
+    var temp;
+    
+    // Loop through the string and store the character code in a temporary variable 
     for (var i = 0; i < str.length; i++) {
-        if(str.charCodeAt(i+1) - str.charCodeAt(i) != 1) {
-            console.log(str.charCodeAt(i));
+        temp = str.charCodeAt(i);
+    
+        // If the end of the string is reached return undefined
+        if (str.length-1 === i) {
+           return undefined;
         }
-        else {
-            console.log(str.charCodeAt(i));
+    
+        // If the following character code is not 1 higher than the current character code, return the missing letter
+        if ((str.charCodeAt(i+1) - temp !== 1)) {
+            return String.fromCharCode(temp+1);
         }
     }
-    return str;
 }
 
-fearNotLetter("abce");
+console.log(fearNotLetter("abdef"));
