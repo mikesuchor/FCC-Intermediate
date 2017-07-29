ca1fc5b372828deca35c7c3e158a2825d841fd65
mikesuchor@gmail.com <mikesuchor@gmail.com>
2017-07-28 22:35:01 -0400

    FCC Intermediate Javascript Exercises

---
+++ Finders Keepers.js
@@ -0,0 +1,14 @@
+/* Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth
+test (second argument).
+*/
+
+function findElement(arr, func) {
+
+  // Filter the array by the function's second value
+  var num = arr.filter(func);
+
+  // Returns the first match
+  return num[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
